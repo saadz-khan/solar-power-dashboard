dc714e4ba05f76e5f7210e440827787128931d30
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-05-04 04:11:07 +0500

    Added second line graph

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ import BatteryChargingFullIcon from "@material-ui/icons/BatteryChargingFull";
 import { ResponsiveLine } from "@nivo/line";
 import { ResponsiveBar } from "@nivo/bar";
 
-import TEST_DATA from "./test_data.json";
-
 const CHART_SIZE = 10;
 
 const useStyles = makeStyles({
@@ -113,6 +111,32 @@ const App = () => {
     ];
   }, [data, offset]);
 
+
+  const powerChartData = useMemo(() => {
+    const slicedData = data.slice(offset, offset + CHART_SIZE); // Use data instead of powerData for X-axis
+    const slicedPowerData = powerData.slice(offset, offset + CHART_SIZE);
+    return [
+      {
+        id: "AC Power",
+        color: "hsl(20, 70%, 50%)",
+        data: slicedData.map((item, index) => ({ // Use slicedData instead of slicedPowerData for X-axis
+          x: item.DATE_TIME,
+          y: slicedPowerData[index]?.AC_POWER ?? 0, // Use index to get AC_POWER from slicedPowerData
+        })),
+      },
+      {
+        id: "DC Power",
+        color: "hsl(200, 70%, 50%)",
+        data: slicedData.map((item, index) => ({ // Use slicedData instead of slicedPowerData for X-axis
+          x: item.DATE_TIME,
+          y: slicedPowerData[index]?.DC_POWER ?? 0, // Use index to get DC_POWER from slicedPowerData
+        })),
+      },
+    ];
+  }, [data, powerData, offset]);
+  
+  
+
   const classes = useStyles();
 
   const renderSummaryCard = (icon, title, value) => (
@@ -153,7 +177,7 @@ const App = () => {
             data={chartData}
             margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
             xScale={{ type: "time", format: "%d/%m/%Y %H:%M" }} //05/15/2020 0:00
-            xFormat="time:%Y-%m-%dT%H:%M:%S.%L%Z"
+            xFormat="time:%d-%m-%Y %H:%M"
             yScale={{
               type: "linear",
               min: "auto",
@@ -214,6 +238,79 @@ const App = () => {
             ]}
           />
         </div>{" "}
+        
+        <div className="chart-wrapper">
+          <button onClick={handleChartBack}>Back</button>
+          <button onClick={handleChartForward}>Forward</button>
+          <div style={{ height: 400 }}>
+            <ResponsiveLine
+              data={powerChartData}
+              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+              xScale={{ type: "time", format: "%d/%m/%Y %H:%M" }} //05/15/2020 0:00
+              xFormat="time:%d-%m-%Y %H:%M"
+              yScale={{
+                type: "linear",
+                min: "auto",
+                max: "auto",
+                stacked: false,
+                reverse: false,
+              }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Time",
+                format: "%d-%m-%Y %H:%M",
+                legendOffset: 36,
+                legendPosition: "middle",
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Power",
+                legendOffset: -40,
+                legendPosition: "middle",
+              }}
+              pointSize={10}
+              pointColor={{ theme: "background" }}
+              pointBorderWidth={2}
+              pointBorderColor={{ from: "serieColor" }}
+              pointLabelYOffset={-12}
+              useMesh={true}
+              legends={[
+                {
+                  anchor: "bottom-right",
+                  direction: "column",
+                  justify: false,
+                  translateX: 100,
+                  translateY: 0,
+                  itemsSpacing: 0,
+                  itemDirection: "left-to-right",
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemOpacity: 0.75,
+                  symbolSize: 12,
+                  symbolShape: "circle",
+                  symbolBorderColor: "rgba(0, 0, 0, .5)",
+                  effects: [
+                    {
+                      on: "hover",
+                      style: {
+                        itemBackground: "rgba(0, 0, 0, .03)",
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
+            />
+          </div>
+        </div>{" "}
+
+
       </div>
     </>
   );
