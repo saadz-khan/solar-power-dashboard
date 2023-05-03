53bd17f0822a136f13d64e17cf8903e55cdfba94
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-05-04 03:25:42 +0500

    dashboard with line graph exists

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { InitializeFirebase, InitializeFirebase1 } from "./firebase";
 import { ref, onValue } from "firebase/database";
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,6 +13,9 @@ import BatteryChargingFullIcon from "@material-ui/icons/BatteryChargingFull";
 import { ResponsiveLine } from "@nivo/line";
 import { ResponsiveBar } from "@nivo/bar";
 
+import TEST_DATA from "./test_data.json";
+
+const CHART_SIZE = 10;
 
 const useStyles = makeStyles({
   card: {
@@ -33,34 +36,10 @@ const useStyles = makeStyles({
   },
 });
 
-
 const App = () => {
   const [data, setData] = useState([]);
-  const [data1, setData1] = useState([]);
-
-  const formatChartData = (data, data1) => {
-    const chartData = data?.map((item) => {
-      return {
-        DATE_TIME: item.data.DATE_TIME,
-        AMBIENT_TEMPERATURE: item.data.AMBIENT_TEMPERATURE,
-        MODULE_TEMPERATURE: item.data.MODULE_TEMPERATURE,
-        IRRADIATION: item.data.IRRADIATION,
-        AC_POWER: data1.find((x) => x.data.DATE_TIME === item.data.DATE_TIME)?.data.AC_POWER,
-        DC_POWER: data1.find((x) => x.data.DATE_TIME === item.data.DATE_TIME)?.data.DC_POWER,
-      };
-    });
-
-  
-    const chartData1 = data1?.map((item) => {
-      return {
-        DATE_TIME: item.data.DATE_TIME,
-        DAILY_YIELD: item.data.DAILY_YIELD,
-        TOTAL_YIELD: item.data.TOTAL_YIELD,
-      };
-    });
-  
-    return { chartData, chartData1 };
-  };
+  const [powerData, setPowerData] = useState([])
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
     const db = InitializeFirebase();
@@ -68,11 +47,10 @@ const App = () => {
     onValue(dbRef, (snapshot) => {
       let dummyData = [];
       snapshot.forEach((child) => {
-        let childId = child.key;
         let childData = child.val();
-        dummyData.push({ Id: childId, data: childData });
+        dummyData.push(childData);
       });
-      setData(dummyData);
+      setData(dummyData.reverse());
     });
   }, []);
 
@@ -82,181 +60,163 @@ const App = () => {
     onValue(dbRef1, (snapshot) => {
       let dummyData1 = [];
       snapshot.forEach((child) => {
-        let childId1 = child.key;
         let childData1 = child.val();
-        dummyData1.push({ Id: childId1, data: childData1 });
+        dummyData1.push(childData1);
       });
-      setData1(dummyData1);
+      setPowerData(dummyData1.reverse());
     });
   }, []);
 
+  const handleChartBack = () => {
+    if (offset > 0 && offset >= CHART_SIZE) {
+      setOffset(offset - CHART_SIZE);
+    } else if (offset > 0) {
+      setOffset(0);
+    }
+  };
+  const handleChartForward = () => {
+    if (offset < data.length - CHART_SIZE) {
+      setOffset(offset + 10);
+    } else {
+      setOffset(data.length - CHART_SIZE);
+    }
+  };
+  console.log(offset);
 
-  const renderChart = (chartData, chartData1) => (
-    <>
-      <div style={{ height: 400 }}>
-        <ResponsiveBar
-          data={[{AC_POWER: 1, DC_POWER: 2}, {AC_POWER: 4, DC_POWER: 6}]}
-          keys={["AC_POWER", "DC_POWER"]}
-          indexBy="DATE_TIME"
-          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-          padding={0.3}
-          valueScale={{ type: "linear" }}
-          indexScale={{ type: "band", round: true }}
-          colors={{ scheme: "nivo" }}
-          borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Date Time",
-            legendPosition: "middle",
-            legendOffset: 32,
-          }}
-          axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Power",
-            legendPosition: "middle",
-            legendOffset: -40,
-          }}
-          labelSkipWidth={12}
-          labelSkipHeight={12}
-          labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-          legends={[
-            {
-              dataFrom: "keys",
-              anchor: "bottom-right",
-              direction: "column",
-              justify: false,
-              translateX: 120,
-              translateY: 0,
-              itemsSpacing: 2,
-              itemWidth: 100,
-              itemHeight: 20,
-              itemDirection: "left-to-right",
-              itemOpacity: 0.85,
-              symbolSize: 20,
-            },
-          ]}
-          animate={true}
-          motionStiffness={90}
-          motionDamping={15}
-        />
-      </div>
-      <div style={{ height: 400 }}>
-        {/* <ResponsiveLine
-          data={chartData1 ?? []}
-          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-          xScale={{ type: "point" }}
-          yScale={{ type: "linear", min: "auto", max: "auto", stacked: false, reverse: false }}
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            orient: "bottom",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Date Time",
-            legendOffset: 36,
-            legendPosition: "middle",
-          }}
-          axisLeft={{
-            orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Yield",
-            legendOffset: -40,
-            legendPosition: "middle",
-          }}
-          colors={{ scheme: "nivo" }}
-          pointSize={10}
-          pointColor={{ theme: "background" }}
-          pointBorderWidth={2}
-          pointBorderColor={{ from: "serieColor" }}
-          pointLabel="y"
-          pointLabelYOffset={-12}
-          useMesh={true}
-          legends={[
-            {
-              anchor: "bottom-right",
-              direction: "column",
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: "left-to-right",
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: "circle",
-              symbolBorderColor: "rgba(0, 0, 0, .5)",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemBackground: "rgba(0, 0, 0, .03)",
-                    itemOpacity: 1,
-                  },
-                },
-              ],
-            },
-          ]}
-        /> */}
-      </div>
-      </>
-    );
-        
-
-  const { chartData, chartData1 } = formatChartData(data, data1);
-  console.log(chartData1)
-
+  const chartData = useMemo(() => {
+    const slicedData = data.slice(offset, offset + CHART_SIZE);
+    return [
+      {
+        id: "Ambient Temperature",
+        color: "hsl(306, 70%, 50%)",
+        data: slicedData.map((item, index) => ({
+          x: item.DATE_TIME,
+          y: item.AMBIENT_TEMPERATURE,
+        })),
+      },
+      {
+        id: "Module Temperature",
+        color: "hsl(234, 70%, 50%)",
+        data: slicedData.map((item, index) => ({
+          x: item.DATE_TIME,
+          y: item.MODULE_TEMPERATURE,
+        })),
+      },
+      {
+        id: "Solar Irradiation",
+        color: "hsl(292, 70%, 50%)",
+        data: slicedData.map((item) => ({
+          x: item.DATE_TIME,
+          y: item.IRRADIATION,
+        })),
+      },
+    ];
+  }, [data, offset]);
 
   const classes = useStyles();
 
-const latestData = data[data.length - 1]?.data;
-const latestData1 = data1[data1.length - 1]?.data;
+  const renderSummaryCard = (icon, title, value) => (
+    <Card className={classes.card}>
+      <CardContent>
+        {icon}
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {title}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {value}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 
-const latestIrradiation = latestData?.IRRADIATION;
-const latestAmbientTemperature = latestData?.AMBIENT_TEMPERATURE;
-const latestModuleTemperature = latestData?.MODULE_TEMPERATURE;
-const latestAcPower = latestData1?.AC_POWER;
-const latestDcPower = latestData1?.DC_POWER;
-const latestDailyYield = latestData1?.DAILY_YIELD;
-const latestTotalYield = latestData1?.TOTAL_YIELD;
 
-const renderSummaryCard = (icon, title, value) => (
-<Card className={classes.card}>
-<CardContent>
-{icon}
-<Typography className={classes.title} color="textSecondary" gutterBottom>
-{title}
-</Typography>
-<Typography variant="h5" component="h2">
-{value}
-</Typography>
-</CardContent>
-</Card>
-);
-
-return (
-  <>
-    <div>
-      {renderSummaryCard(<WbSunnyIcon />, "Irradiation", latestIrradiation)}
-      {renderSummaryCard(<AcUnitIcon />, "Ambient Temperature", latestAmbientTemperature)}
-      {renderSummaryCard(<SpeedIcon />, "Module Temperature", latestModuleTemperature)}
-      {renderSummaryCard(<PowerIcon />, "AC Power", latestAcPower)}
-      {renderSummaryCard(<PowerIcon />, "DC Power", latestDcPower)}
-      {renderSummaryCard(<BatteryChargingFullIcon />, "Daily Yield", latestDailyYield)}
-      {renderSummaryCard(<BatteryChargingFullIcon />, "Total Yield", latestTotalYield)}
+  return (
+    <>
+        <div>
+      {renderSummaryCard(<WbSunnyIcon />, "Irradiation", data[0]?.IRRADIATION ?? '-')}
+      {renderSummaryCard(<AcUnitIcon />, "Ambient Temperature", data[0]?.AMBIENT_TEMPERATURE ?? '-')}
+      {renderSummaryCard(<SpeedIcon />, "Module Temperature", data[0]?.MODULE_TEMPERATURE ?? '-')}
+      {renderSummaryCard(<PowerIcon />, "AC Power", powerData[0]?.AC_POWER ?? '-')}
+      {renderSummaryCard(<PowerIcon />, "DC Power", powerData[0]?.DC_POWER ?? '-')}
+      {renderSummaryCard(<BatteryChargingFullIcon />, "Daily Yield", powerData[0]?.DAILY_YIELD ?? '-')}
+      {renderSummaryCard(<BatteryChargingFullIcon />, "Total Yield", powerData[0]?.TOTAL_YIELD ?? '-')}
     </div>
-    {renderChart(chartData, chartData1)}
-  </>
-);
+      <div className="chart-wrapper">
+        <button onClick={handleChartBack}>Back</button>
+        <button onClick={handleChartForward}>Forward</button>
+        <div style={{ height: 400 }}>
+          <ResponsiveLine
+            data={chartData}
+            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+            xScale={{ type: "time", format: "%d/%m/%Y %H:%M" }} //05/15/2020 0:00
+            xFormat="time:%Y-%m-%dT%H:%M:%S.%L%Z"
+            yScale={{
+              type: "linear",
+              min: "auto",
+              max: "auto",
+              stacked: false,
+              reverse: false,
+            }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "Time",
+              format: "%d-%m-%Y %H:%M",
+              legendOffset: 36,
+              legendPosition: "middle",
+            }}
+            axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "count",
+              legendOffset: -40,
+              legendPosition: "middle",
+            }}
+            pointSize={10}
+            pointColor={{ theme: "background" }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: "serieColor" }}
+            pointLabelYOffset={-12}
+            useMesh={true}
+            legends={[
+              {
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 100,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>{" "}
+      </div>
+    </>
+  );
 };
 
 export default App;
