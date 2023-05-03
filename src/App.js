29ef2bc1ff6a7cfb925d83c7b0153be0b09c09b4
Saad Zafar Khan <szkhan.bee19@gmail.com>
2023-05-04 01:48:02 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { InitializeFirebase, InitializeFirebase1 } from "./firebase";
 import { ref, onValue } from "firebase/database";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  BarChart,
-  Bar,
-  ComposedChart,
-  Area,
-} from "recharts";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import WbSunnyIcon from "@material-ui/icons/WbSunny";
-import OpacityIcon from "@material-ui/icons/Opacity";
-import PowerIcon from "@material-ui/icons/Power";
-import SpeedIcon from "@material-ui/icons/Speed";
 import AcUnitIcon from "@material-ui/icons/AcUnit";
+import SpeedIcon from "@material-ui/icons/Speed";
+import PowerIcon from "@material-ui/icons/Power";
 import BatteryChargingFullIcon from "@material-ui/icons/BatteryChargingFull";
+import { ResponsiveLine } from "@nivo/line";
+import { ResponsiveBar } from "@nivo/bar";
 
 
 const useStyles = makeStyles({
@@ -40,6 +27,10 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  chartWrapper: {
+    height: "400px",
+    marginBottom: "50px",
+  },
 });
 
 
@@ -48,7 +39,7 @@ const App = () => {
   const [data1, setData1] = useState([]);
 
   const formatChartData = (data, data1) => {
-    const chartData = data.map((item) => {
+    const chartData = data?.map((item) => {
       return {
         DATE_TIME: item.data.DATE_TIME,
         AMBIENT_TEMPERATURE: item.data.AMBIENT_TEMPERATURE,
@@ -60,7 +51,7 @@ const App = () => {
     });
 
   
-    const chartData1 = data1.map((item) => {
+    const chartData1 = data1?.map((item) => {
       return {
         DATE_TIME: item.data.DATE_TIME,
         DAILY_YIELD: item.data.DAILY_YIELD,
@@ -82,10 +73,6 @@ const App = () => {
         dummyData.push({ Id: childId, data: childData });
       });
       setData(dummyData);
-
-      dummyData.map((child, index) => {
-        console.log(index, " : ", child);
-      });
     });
   }, []);
 
@@ -100,14 +87,134 @@ const App = () => {
         dummyData1.push({ Id: childId1, data: childData1 });
       });
       setData1(dummyData1);
-
-      dummyData1.map((child, index) => {
-        console.log(index, " : ", child);
-      });
     });
   }, []);
 
+
+  const renderChart = (chartData, chartData1) => (
+    <>
+      <div style={{ height: 400 }}>
+        <ResponsiveBar
+          data={[{AC_POWER: 1, DC_POWER: 2}, {AC_POWER: 4, DC_POWER: 6}]}
+          keys={["AC_POWER", "DC_POWER"]}
+          indexBy="DATE_TIME"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.3}
+          valueScale={{ type: "linear" }}
+          indexScale={{ type: "band", round: true }}
+          colors={{ scheme: "nivo" }}
+          borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Date Time",
+            legendPosition: "middle",
+            legendOffset: 32,
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Power",
+            legendPosition: "middle",
+            legendOffset: -40,
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+          legends={[
+            {
+              dataFrom: "keys",
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: "left-to-right",
+              itemOpacity: 0.85,
+              symbolSize: 20,
+            },
+          ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+        />
+      </div>
+      <div style={{ height: 400 }}>
+        {/* <ResponsiveLine
+          data={chartData1 ?? []}
+          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          xScale={{ type: "point" }}
+          yScale={{ type: "linear", min: "auto", max: "auto", stacked: false, reverse: false }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            orient: "bottom",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Date Time",
+            legendOffset: 36,
+            legendPosition: "middle",
+          }}
+          axisLeft={{
+            orient: "left",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Yield",
+            legendOffset: -40,
+            legendPosition: "middle",
+          }}
+          colors={{ scheme: "nivo" }}
+          pointSize={10}
+          pointColor={{ theme: "background" }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: "serieColor" }}
+          pointLabel="y"
+          pointLabelYOffset={-12}
+          useMesh={true}
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: "left-to-right",
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: "circle",
+              symbolBorderColor: "rgba(0, 0, 0, .5)",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemBackground: "rgba(0, 0, 0, .03)",
+                    itemOpacity: 1,
+                  },
+                },
+              ],
+            },
+          ]}
+        /> */}
+      </div>
+      </>
+    );
+        
+
   const { chartData, chartData1 } = formatChartData(data, data1);
+  console.log(chartData1)
+
 
   const classes = useStyles();
 
@@ -137,45 +244,18 @@ const renderSummaryCard = (icon, title, value) => (
 );
 
 return (
-<>
-<div>
-{renderSummaryCard(<WbSunnyIcon />, "Irradiation", latestIrradiation)}
-{renderSummaryCard(<AcUnitIcon />, "Ambient Temperature", latestAmbientTemperature)}
-{renderSummaryCard(<SpeedIcon />, "Module Temperature", latestModuleTemperature)}
-{renderSummaryCard(<PowerIcon />, "AC Power", latestAcPower)}
-{renderSummaryCard(<PowerIcon />, "DC Power", latestDcPower)}
-{renderSummaryCard(<BatteryChargingFullIcon />, "Daily Yield", latestDailyYield)}
-{renderSummaryCard(<BatteryChargingFullIcon />, "Total Yield", latestTotalYield)}
-</div>
-
-<ResponsiveContainer width="100%" height={400}>
-    <ComposedChart data={chartData}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="DATE_TIME" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="AC_POWER" fill="#8884d8" />
-      <Bar dataKey="DC_POWER" fill="#82ca9d" />
-      <Line type="monotone" dataKey="AMBIENT_TEMPERATURE" stroke="#ff7300" />
-      <Line type="monotone" dataKey="MODULE_TEMPERATURE" stroke="#fdd200" />
-      <Line type="monotone" dataKey="IRRADIATION" stroke="#387908" />
-    </ComposedChart>
-  </ResponsiveContainer>
-
-  <ResponsiveContainer width="100%" height={400}>
-    <LineChart data={chartData1} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="DATE_TIME" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="DAILY_YIELD" stroke="#8884d8" />
-      <Line type="monotone" dataKey="TOTAL_YIELD" stroke="#82ca9d" />
-    </LineChart>
-  </ResponsiveContainer>
-</>
-
+  <>
+    <div>
+      {renderSummaryCard(<WbSunnyIcon />, "Irradiation", latestIrradiation)}
+      {renderSummaryCard(<AcUnitIcon />, "Ambient Temperature", latestAmbientTemperature)}
+      {renderSummaryCard(<SpeedIcon />, "Module Temperature", latestModuleTemperature)}
+      {renderSummaryCard(<PowerIcon />, "AC Power", latestAcPower)}
+      {renderSummaryCard(<PowerIcon />, "DC Power", latestDcPower)}
+      {renderSummaryCard(<BatteryChargingFullIcon />, "Daily Yield", latestDailyYield)}
+      {renderSummaryCard(<BatteryChargingFullIcon />, "Total Yield", latestTotalYield)}
+    </div>
+    {renderChart(chartData, chartData1)}
+  </>
 );
 };
 
